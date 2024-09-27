cb5c11244d729b010ab5a3e123cb3eb3bded1ac8
Murilo Verissimo Pereira <143817507+verissimoMuh@users.noreply.github.com>
2024-09-27 07:49:30 -0300

    Add files via upload

---
+++ PAM-CALCULO/calculoUm.js
@@ -0,0 +1,33 @@
+
+function CalculoAdicao(){
+  var n = 'Adição:';  
+ resultado = 11+23 ;
+ console.log(n);
+ console.log(resultado);
+}
+
+ console.log(CalculoAdicao());
+
+ function CalculoSubtracao(){
+    var n = 'Subtração:';  
+    resultado = 145-87 ;
+    console.log(n);
+    console.log(resultado); }
+   
+console.log(CalculoSubtracao());
+
+function CalculoDivisao(){
+    var n = 'Divisão:';  
+    resultado = 145/5 ;
+    console.log(n);
+    console.log(resultado); }
+   
+console.log(CalculoDivisao());
+
+function CalculoMultiplicacao(){
+    var n = 'Multiplicação:';  
+    resultado = 145*27 ;
+    console.log(n);
+    console.log(resultado); }
+   
+console.log(CalculoMultiplicacao());
